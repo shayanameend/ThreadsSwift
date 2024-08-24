793308de3b976536d70bc19adcd50d753047819b
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-08-24 17:47:17 +0500

    App Setup

---
+++ Threads/App/ThreadsApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ Threads/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct LoginView: View {
+  var body: some View {
+    NavigationStack {
+      VStack {
+        Image("threads-app-icon")
+      }
+    }
+  }
+}
+
+#Preview {
+  LoginView()
+}
