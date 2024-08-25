f17995ab9471caa6064fc8a9d32ae77bb6624ea8
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-08-25 10:46:22 +0500

    Add navigation and authentication fields to LoginView
    This commit introduces new state properties for email and password in the LoginView and enhances the UI with additional fields and navigation options. It also updates ContentView to incorporate a NavigationStack for better navigation management. Additionally, configuration files for project setup are added and minor adjustments to scheme management are made.

--- Threads/Core/Authentication/Views/LoginView.swift
+++ Threads/Core/Authentication/Views/LoginView.swift
@@ -1,10 +1,66 @@
 import SwiftUI
 
 struct LoginView: View {
+  @State private var email = ""
+  @State private var password = ""
+
   var body: some View {
     NavigationStack {
       VStack {
+        Spacer()
+
         Image("threads-app-icon")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 120, height: 120)
+          .padding()
+
+        VStack {
+          TextField("Email", text: $email)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+          SecureField("Password", text: $password)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+
+        NavigationLink {
+          Text("Forgot Password View")
+        } label: {
+          Text("Forgot Password?")
+            .font(.footnote)
+            .fontWeight(.semibold)
+            .padding(.top)
+            .padding(.trailing, 20)
+            .foregroundColor(.black)
+            .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+
+        Button {} label: {
+          Text("Login")
+            .font(.headline)
+            .fontWeight(.semibold)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 352, height: 44)
+            .background(.black)
+            .cornerRadius(10)
+        }
+
+        Spacer()
+
+        Divider()
+
+        NavigationLink {
+          Text("Registeration View")
+        } label: {
+          HStack(spacing: 3) {
+            Text("Don't have an account?")
+            Text("Sign Up")
+              .fontWeight(.bold)
+          }
+          .font(.footnote)
+          .foregroundColor(.black)
+        }
+        .padding(.vertical, 16)
       }
     }
   }
--- Threads/Core/Root/ContentView.swift
+++ Threads/Core/Root/ContentView.swift
@@ -2,7 +2,14 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, World!")
+    NavigationStack {
+      NavigationLink {
+        Text("Hello, World")
+      } label: {
+        Text("Hello, World")
+      }
+      .navigationBarTitle("Threads")
+    }
   }
 }
 
